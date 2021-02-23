a108aa7952b71bd389d7f7262bfe8ad9f571721e
raturusi <raturusi@mts.ru>
2021-02-23 19:27:01 +0300

    Test

---
+++ les1_new.py
@@ -0,0 +1,49 @@
+import time
+import json
+from pathlib import Path
+import requests
+
+class Parse5Ka:
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+    }
+
+    def __init__(self, start_url: str, products_path: Path):
+        self.start_url = start_url
+        self.products_path = products_path
+
+    def _get_response(self, url):
+        while True:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                return response
+            time.sleep(0.5)
+
+    def run(self):
+        for product in self._parse(self.start_url):
+            product_path = self.products_path.joinpath(f"{product['id']}.json")
+            self._save(product, product_path)
+
+    def _parse(self, url):
+        while url:
+            response = self._get_response(url)
+            data = response.json()
+            url = data["next"]
+            for product in data["results"]:
+                yield product
+
+    @staticmethod
+    def _save(data: dict, file_path):
+        jdata = json.dumps(data, ensure_ascii=False)
+        file_path.write_text(jdata, encoding="UTF-8")
+
+
+if __name__ == "__main__":
+    url = "https://5ka.ru/api/v2/special_offers/"
+    save_path = Path(__file__).parent.joinpath("products")
+    if not save_path.exists():
+        save_path.mkdir()
+
+parser = Parse5Ka(url, save_path)
+parser.run()
